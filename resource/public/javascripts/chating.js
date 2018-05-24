9515033f7ba6c85b791c0a965807bb4e54a1d032
CA180004 <thtn0333@gmail.com>
2018-05-24 17:36:41 +0900

    [modify] Bug Fix

--- resource/public/javascripts/chating.js
+++ resource/public/javascripts/chating.js
@@ -5,6 +5,32 @@ $.ajax({
     type: "get",
     success: function (result) {
         successResult=result;
+        $(function () {
+
+            var socket=io();
+
+            socket.emit('login',{
+                name:successResult[0].nickname,
+                userid:successResult[0].id
+            });
+
+            socket.on("login", function(data) {
+                $("#chatLogs").append("<div><strong>" + data + "</strong> has joined</div>");
+            });
+
+            socket.on("chat", function(data) {
+                $("#chatLogs").append("<div>" + data.msg + " : from <strong>" + data.from.name + "</strong></div>");
+            });
+
+            $("form").submit(function(e) {
+                e.preventDefault();
+                var $msgForm = $("#msgForm");
+
+                // 서버로 메시지를 전송한다.
+                socket.emit("chat", { msg: $msgForm.val() });
+                $msgForm.val("");
+            });
+        })
     },
     error: function (err) {
         alert("Access Error");
@@ -12,29 +38,3 @@ $.ajax({
     }
 });
 
-$(function () {
-
-    var socket=io();
-
-    socket.emit('login',{
-        name:successResult[0].nickname,
-        userid:successResult[0].id
-    });
-
-    socket.on("login", function(data) {
-        $("#chatLogs").append("<div><strong>" + data + "</strong> has joined</div>");
-    });
-
-    socket.on("chat", function(data) {
-        $("#chatLogs").append("<div>" + data.msg + " : from <strong>" + data.from.name + "</strong></div>");
-    });
-
-    $("form").submit(function(e) {
-        e.preventDefault();
-        var $msgForm = $("#msgForm");
-
-        // 서버로 메시지를 전송한다.
-        socket.emit("chat", { msg: $msgForm.val() });
-        $msgForm.val("");
-    });
-})
--- resource/public/javascripts/main.js
+++ resource/public/javascripts/main.js
@@ -1,32 +1,36 @@
-var successResult;
+var successResult=[];
+
+$.ajax({
+    url: "/api/v1/users/token/" + localStorage.getItem("token"),
+    type: "get",
+    success: function (result) {
+        successResult=result;
+        alert("nope");
+        $(function () {
+            alert("ready");
+            $('#playerName').append(successResult[0].nickname);
+            $('#playerScore').append(successResult[0].score);
+
+            $("#rankButton").on("click", () => {
+                location.href = "/ranking";
+            });
+            $("#playButton").on("click", () => {
+                location.href = "/play";
+            });
+            $("#chatButton").on("click", () => {
+                location.href = "/chating";
+            });
 
 
-    $.ajax({
-        url: "/api/v1/users/token/" + localStorage.getItem("token"),
-        type: "get",
-        success: function (result) {
-            successResult=result;
-        },
-        error: function (err) {
-            alert("Access Error");
-            location.href = "/";
-        }
-    });
-
-
-
-$(function () {
-    $('#playerName').append(successResult[0].nickname);
-    $('#playerScore').append(successResult[0].score);
-    $("#rankButton").on("click", () => {
-        location.href = "/ranking";
-    });
-    $("#playButton").on("click", () => {
-        location.href = "/play";
-    });
-
-    $("#logoutButton").on("click", () => {
-        localStorage.removeItem("token");
+            $("#logoutButton").on("click", () => {
+                localStorage.removeItem("token");
+                location.href = "/";
+            });
+        });
+    },
+    error: function (err) {
+        alert("Access Error");
         location.href = "/";
-    });
+    }
 });
+
